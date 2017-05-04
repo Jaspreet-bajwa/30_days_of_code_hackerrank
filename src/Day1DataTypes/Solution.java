322bad33de04d0e206fa20f0647bba8b1cf6fa2e
Jaspreet-bajwa <JazBajwa@172.16.105.229>
2017-05-03 20:51:27 -0400

    day 1 commit

--- src/Day1DataTypes/Solution.java
+++ src/Day1DataTypes/Solution.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 public class Solution {
 
+	private static Scanner scan;
+
 	public static void main(String[] args) {
 		int i = 4;
 		double d = 4.0;
 		String s = "HackerRank ";
 
-		Scanner scan = new Scanner(System.in);
+		scan = new Scanner(System.in);
 
 		/* Declare second integer, double, and String variables. */
 		int j = scan.nextInt();
